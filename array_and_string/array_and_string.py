0f47b52b26357939f97ffbc854dcc7eec253bbbc
aramdeveloper1 <aramdeveloper1@gmail.com>
2025-02-18 16:38:04 +0300

    matrix -row -colum zero

--- array_and_string/array_and_string.py
+++ array_and_string/array_and_string.py
@@ -204,10 +204,39 @@ def rotate_matrix(matrix):
         matrix[i].reverse()
 
 # Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 0, 6],
+#     [7, 8, 9]
+# ]
 # rotate_matrix(matrix)
 # print(matrix)
+def zero_matrix(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    zero_rows = set()
+    zero_cols = set()
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:
+                zero_rows.add(i)
+                zero_cols.add(j)
+    print("zero_rows *",zero_rows)
+    print("zero_cols *",zero_cols)
+    for i in range(rows):
+        for j in range(cols):
+            if i in zero_rows or j in zero_cols:
+                print("i *",i)
+                print("j *",j)
+                print("matrix[i][j] *",matrix[i][j])
+                matrix[i][j] = 0
+    return matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 0, 6],
+    [7, 8, 9]
+]
+# print(zero_matrix(matrix))
+# # print(matrix)
